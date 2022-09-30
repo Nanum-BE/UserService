608da2d1aa057eeddf8caba232f289871fdb320a
mins00oo <alstn7223@gmail.com>
2022-09-30 15:22:50 +0900

    crossorigin설정 추가

--- src/main/java/com/nanum/userservice/user/presentation/UserController.java
+++ src/main/java/com/nanum/userservice/user/presentation/UserController.java
@@ -37,6 +37,7 @@ import java.util.List;
 @Tag(name = "사용자", description = "사용자 관련 api")
 @Slf4j
 @RequiredArgsConstructor
+@CrossOrigin
 @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = "success",
                 content = @Content(schema = @Schema(implementation = BaseResponse.class))),
